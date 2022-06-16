1fdb4562f94125d30890a423be86157c2b429737
Oren <orennoyes@Administrators-MacBook-Pro.local>
2022-06-15 19:12:05 -0600

    commit tree 54031738c2343cc5a73e3b3573ea60519e294d5e

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,11 @@
 
-describe('shout(string)', function() {
+describe('shout(string)', function shout(string) {
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
   })
 })
 
-describe('whisper(string)', function() {
+describe('whisper(string)', function whisper(string) {
   it('receives one argument and returns it in all lowercase', function() {
     expect(whisper('HELLO')).toEqual('hello');
   })
